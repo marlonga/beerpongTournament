ff6a5ceef91650f0ee383ccf1d32e5bc2931dbc9
Marlon <marlon.gaethje@web.de>
2022-04-21 19:59:43 +0200

    bugfixes

--- Beerpong/src/StartPanel.java
+++ Beerpong/src/StartPanel.java
@@ -9,7 +9,6 @@ public class StartPanel extends JPanel {
     private static ArrayList<JLabel> labelSpieler = new ArrayList<>();
     private static int spielerzahl = 0;
     private int playercount = 0;
-    
     public StartPanel() {
         setLayout(null);
         setBounds(0,0,500,500);
@@ -18,6 +17,29 @@ public class StartPanel extends JPanel {
         namenseingabe.addActionListener(actionEingabeDerNamen);
         add(namenseingabe);
         updateUI();
+        System.out.println("ahshdhha");System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+        System.out.println("ahshdhha");
+
+
+
+
     }
 
     public static JLabel getPlayers(int x){
