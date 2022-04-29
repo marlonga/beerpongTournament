d85688cab7d1cc5891aab2d2e2c023528d1b316a
Marlon <marlon.gaethje@web.de>
2022-04-29 21:04:58 +0200

    bugfixes

---
+++ Beerpong/src/GameLogic.java
@@ -0,0 +1,70 @@
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class GameLogic {
+    ArrayList<String> players;
+
+    //NOCH TESTEN!!!!!!!
+
+    GameLogic(ArrayList<String> players){
+        this.players = players;
+    }
+
+    public static class matches {
+        String player1;
+        String player2;
+
+        matches(String player1,String player2){
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public void setPlayer1(String player1) {
+            this.player1 = player1;
+        }
+
+        public void setPlayer2(String player2) {
+            this.player2 = player2;
+        }
+
+        @Override
+        public String toString(){
+            return player1 + player2;
+        }
+    }
+
+
+
+    public static class ranking {
+        String player;
+        int points;
+        ranking(String player,int points){
+            this.player = player;
+            this.points = points;
+        }
+        public void increment(){
+            this.points++;
+        }
+    }
+
+
+
+    public ArrayList<matches> allMatches(){
+        ArrayList<matches> match = new ArrayList<>();
+        for(int i = 0;i<players.size();i++){
+            for(int j = 1;j< players.size();j++) {
+                matches temp = new matches(players.get(i), players.get(j));
+                match.add(temp);
+            }
+            players.remove(players.get(i));
+        }
+        for(int i=0;i<match.size();i++){
+            System.out.println(match.toString());
+        }
+        return null;
+    }
+
+
+
+
+}
