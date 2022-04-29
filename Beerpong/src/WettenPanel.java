9c12b0289d655abe01090d14f1ee1c6186ddac77
Marlon <marlon.gaethje@web.de>
2022-04-29 16:01:01 +0200

    bugfixes

--- Beerpong/src/WettenPanel.java
+++ Beerpong/src/WettenPanel.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class WettenPanel extends JPanel {
     private ArrayList<String> players;
+    private ArrayList<JLabel> playerLabels;
+    private ArrayList<JButton> blueButtons;
+    private ArrayList<JButton> redButtons;
     private String player1;
     private String player2;
 
@@ -11,8 +15,27 @@ public class WettenPanel extends JPanel {
         this.player1 = player1;
         this.player2 = player2;
         setBounds(x,y,200,500);
-        for(int i = 0; i<players.size();i++){
+        setMaximumSize(new Dimension(200,500));
+        setMinimumSize(new Dimension(200,500));
+        draw();
+    }
 
+    public void draw(){
+        for(int i = 0; i<players.size();i++){
+            if(!(players.contains(player1) || players.contains(player2))){
+                playerLabels.add(new JLabel(players.get(i)));
+                add(playerLabels.get(i));
+                playerLabels.get(i).setBounds(20,20*i,100,20);
+
+                blueButtons.add(new JButton("blue"));
+                add(blueButtons.get(i));
+                blueButtons.get(i).setBounds(60,20*i,20,20);
+
+                redButtons.add(new JButton("red"));
+                add(redButtons.get(i));
+                redButtons.get(i).setBounds(90,20*i,20,20);
+
+            }
         }
     }
 
